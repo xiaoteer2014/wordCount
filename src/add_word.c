bc628bb914de4a96c2c4b3b0fe74eeb6d8e41200
xiaoteer <xiaoteer2014@163.com>
2015-08-03 21:47:29 +0800

    完成了初版

--- src/add_word.c
+++ src/add_word.c
@@ -6,4 +6,41 @@
  ************************************************************************/
 
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"word_manage_p.h"
 
+static void shift_array(int index) {
+    int src;
+    
+    for (src = num_of_word - 1; src >= index; src--)
+        word_array[src + 1] = word_array[src];
+    num_of_word++;
+}
+
+static char *my_strdup(char *src) {
+    char *dest;
+    
+    dest = malloc(sizeof(char) * (strlen(src) + 1));
+    strcpy(dest, src);
+
+    return dest;
+}
+
+void add_word(char *word) {
+    int i;
+    int result;
+
+    for (i = 0; i < num_of_word; i++) {
+        result = strcmp(word_array[i].name, word);
+        if (result >= 0)
+            break;
+    }
+    if (num_of_word != 0 && result == 0)
+        word_array[i].count++;
+    else {
+        shift_array(i);
+        word_array[i].name = my_strdup(word);
+        word_array[i].count = 1;
+    }
+}
---
+++ src/get_word.c
@@ -0,0 +1,31 @@
+/*************************************************************************
+	> File Name: get_word.c
+	> Author: 
+	> Mail: 
+	> Created Time: Mon 03 Aug 2015 09:20:51 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+
+int get_word(char *buf, int buf_size, FILE *fp) {
+    int len;
+    int ch;
+
+    while ((ch = getc(fp)) != EOF && !isalnum(ch))
+        ;
+    if (ch == EOF)
+        return EOF;
+
+    len = 0;
+    do {
+        buf[len] = ch;
+        len++;
+        if (len >=buf_size) {
+            fprintf(stderr, "word too long.\n");
+            exit(1);
+        }
+    } while ((ch = getc(fp)) != EOF && isalnum(ch));
+    buf[len] = '\0';
+}
