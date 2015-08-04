7a80a7fc209b16cbe1c02dbc73e736d4ebf9f7d6
xiaoteer2014 <xiaoteer2014@163.com>
2015-08-04 09:31:09 +0800

    格式化

--- src/get_word.c
+++ src/get_word.c
@@ -23,7 +23,7 @@ int get_word(char *buf, int buf_size, FILE *fp) {
     do {
         buf[len] = ch;
         len++;
-        if (len >=buf_size) {
+        if (len >= buf_size) {
             fprintf(stderr, "word too long.\n");
             exit(1);
         }
